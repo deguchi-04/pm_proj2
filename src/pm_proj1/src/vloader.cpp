e9f52e9e60de428c1102c49c6f15e2dc9fda6789
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2022-12-08 14:03:44 +0000

    Add files via upload

--- src/pm_proj1/src/vloader.cpp
+++ src/pm_proj1/src/vloader.cpp
@@ -1,60 +1,18 @@
 #include "pm_proj1.h"
-#include <sstream>
-// declaring namespaces
-using namespace std;
-using namespace cv;
 
-// Trackbars
-static void on_low_H_thresh_trackbar(int, void *)
+geometry_msgs::Vector3 center_tracked;
+
+void cbPose_track(const geometry_msgs::Vector3 &msg)
 {
-    low_H = min(high_H - 1, low_H);
-    setTrackbarPos("Low H", window_detection_name, low_H);
-}
-static void on_high_H_thresh_trackbar(int, void *)
-{
-    high_H = max(high_H, low_H + 1);
-    setTrackbarPos("High H", window_detection_name, high_H);
-}
-static void on_low_S_thresh_trackbar(int, void *)
-{
-    low_S = min(high_S - 1, low_S);
-    setTrackbarPos("Low S", window_detection_name, low_S);
-}
-static void on_high_S_thresh_trackbar(int, void *)
-{
-    high_S = max(high_S, low_S + 1);
-    setTrackbarPos("High S", window_detection_name, high_S);
-}
-static void on_low_V_thresh_trackbar(int, void *)
-{
-    low_V = min(high_V - 1, low_V);
-    setTrackbarPos("Low V", window_detection_name, low_V);
-}
-static void on_high_V_thresh_trackbar(int, void *)
-{
-    high_V = max(high_V, low_V + 1);
-    setTrackbarPos("High V", window_detection_name, high_V);
+    // Received center
+    // FAZER direiro o subscriber
+    center_tracked.x = msg.x;
+    center_tracked.y = msg.y;
+    ROS_WARN_STREAM("Inside callback tracker!");
+    ROS_WARN_STREAM("center=(" << msg.x << "," << msg.y << ")");
 }
 
 /////////////////////////EXERCISE 1/////////////////////////////////////
-
-// FIND CONTOURS OF BALL
-Mat met(Mat frame, int thresh)
-{
-    Mat canny_output;
-    Canny(frame, canny_output, thresh, thresh * 2);
-    vector<vector<Point>> contours;
-    vector<Vec4i> hierarchy;
-    findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-    Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-    for (size_t i = 0; i < contours.size(); i++)
-    {
-        Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-        drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
-    }
-
-    return drawing;
-}
 
 int main(int argc, char **argv)
 {
@@ -64,70 +22,75 @@ int main(int argc, char **argv)
 
     ros::NodeHandle n_private("~");
 
-    n_private.param<int>("meanColor", meanColor, 0);
-    n_private.param<int>("stdev", stdev, 0);
-    n_private.param<string>("path", path, "../project_pm/src/pm_proj1/src/videoPlastic.mp4");
+    n_private.param<int>("lowH", lowH, 0);
+    n_private.param<int>("lowS", lowS, 0);
+    n_private.param<int>("lowV", lowV, 0);
+    n_private.param<int>("highH", highH, 180);
+    n_private.param<int>("highS", highS, 255);
+    n_private.param<int>("highV", highV, 255);
+    n_private.param<std::string>("path", path, "../project_pm/src/pm_proj1/src/videoPlastic.mp4");
 
     // Publishers
     ros::NodeHandle n_frame;
     ros::NodeHandle n_center;
+    ros::NodeHandle n_center_tracked;
+
+    ros::Subscriber sub = n_center_tracked.subscribe("vtracker/ball_center_tracker", 1, cbPose_track);
+
     image_transport::ImageTransport it(n_frame);
+
     ros::Publisher center_ball_publisher;
 
-    image_transport::Publisher frame_publisher = it.advertise("vloarder/frame", 1);
+    image_transport::Publisher frame_publisher;
 
+    frame_publisher = it.advertise("vloarder/frame", 1);
     center_ball_publisher = n_center.advertise<geometry_msgs::Vector3>("vloarder/ball_center", 1000);
     ros::Rate loop_rate(10);
 
-    
-
     // save videos
-    string name;
+    
     if (path == "../project_pm/src/pm_proj1/src/videoPlastic.mp4")
     {
         name = "p1_plastic.mp4";
+        name2 = "p2_plastic.mp4";
     }
     else if (path == "../project_pm/src/pm_proj1/src/videoTennis.mp4")
     {
         name = "p1_tennis.mp4";
+        name2 = "p2_tennis.mp4";
     }
     int count = 0;
-  
 
-        // Open video
-        cv::VideoCapture cap(path);
+    // Open video
+    cv::VideoCapture cap(path);
 
-        // Check if video opened successfully
-        if (!cap.isOpened())
-        {
-            std::cout << "Error opening video stream or file" << std::endl;
-            return -1;
-        }
+    // Check if video opened successfully
+    if (!cap.isOpened())
+    {
+        std::cout << "Error opening video stream or file" << std::endl;
+        return -1;
+    }
 
-        // naming windows
-        namedWindow(window_capture_name, WINDOW_NORMAL);
-        namedWindow(window_detection_name, WINDOW_NORMAL);
-        namedWindow(window_cont_name, WINDOW_NORMAL);
+    // naming windows
+    cv::namedWindow(window_capture_name, cv::WINDOW_NORMAL);
+    cv::namedWindow(window_detection_name, cv::WINDOW_NORMAL);
+    cv::namedWindow(window_cont_name, cv::WINDOW_NORMAL);
 
-        // Trackbars to set thresholds for HSV values
-        createTrackbar("Low H", window_detection_name, &low_H, max_value_H, on_low_H_thresh_trackbar);
-        createTrackbar("High H", window_detection_name, &high_H, max_value_H, on_high_H_thresh_trackbar);
-        createTrackbar("Low S", window_detection_name, &low_S, max_value, on_low_S_thresh_trackbar);
-        createTrackbar("High S", window_detection_name, &high_S, max_value, on_high_S_thresh_trackbar);
-        createTrackbar("Low V", window_detection_name, &low_V, max_value, on_low_V_thresh_trackbar);
-        createTrackbar("High V", window_detection_name, &high_V, max_value, on_high_V_thresh_trackbar);
+    // declarations open cv
+    cv::Mat frame, frame_HSV, frame_threshold, cont;
+    int fps = 30;
 
-        // declarations open cv
-        Mat frame, frame_HSV, frame_threshold, cont;
-        int fps = 30;
+    // Acquire input size
+    cv::Size frame_size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
 
-        // Acquire input size
-        cv::Size frame_size = cv::Size((int)cap.get(cv::CAP_PROP_FRAME_WIDTH), (int)cap.get(cv::CAP_PROP_FRAME_HEIGHT));
+    cv::VideoWriter writer(name, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), fps, frame_size);
+    
+    cv::VideoWriter writer2(name2, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), fps, frame_size);
 
-        cv::VideoWriter writer(name, cv::VideoWriter::fourcc('a', 'v', 'c', '1'), fps, frame_size);
-
-        cv::Scalar color = cv::Scalar(0, 0, 50);
-  while (ros::ok)
+    cv::Scalar color = cv::Scalar(0, 255, 0);
+    cv::Scalar color2 = cv::Scalar(255, 0, 0);
+    cv::Scalar color3 = cv::Scalar(0, 0, 255);
+    while (ros::ok)
     {
         // Iterate through frames
         while (cap.isOpened())
@@ -136,82 +99,111 @@ int main(int argc, char **argv)
             ///////////////////Alinea A//////////////////////
             // Capture next frame
             cap >> frame;
-
+            double area = 0;
             // If the frame is empty, break immediately
             if (frame.empty())
             {
                 break;
             }
+            cv::Mat prin;
+            sensor_msgs::ImagePtr msg_frame = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+            frame_publisher.publish(msg_frame);
             ////////////////////Alinea B//////////////////////
             // Convert from BGR to HSV colorspace
-            cvtColor(frame, frame_HSV, COLOR_BGR2HSV);
+            cvtColor(frame, frame_HSV, cv::COLOR_BGR2HSV);
             // Detect the object based on HSV Range Values
-            inRange(frame_HSV, Scalar(low_H, low_S, low_V), Scalar(high_H, high_S, high_V), frame_threshold);
+            
+            inRange(frame_HSV, cv::Scalar(lowH, lowS, lowV), cv::Scalar(highH, highS, highV), frame_threshold);
+            prin=frame_threshold.clone();
             geometry_msgs::Vector3 center;
+            cv::bitwise_and(frame,frame,prin,frame_threshold);
             ///////////////////// Alinea D //////////////////////////////
             cv::Moments m = moments(frame_threshold, false);
             cv::Point com(m.m10 / m.m00, m.m01 / m.m00);
             cv::drawMarker(frame, com, color, cv::MARKER_CROSS, 20, 5);
             cv::drawMarker(frame_threshold, com, color, cv::MARKER_CROSS, 20, 5);
-
+            cv::putText(frame, "Obs", cv::Point(com.x - 0, com.y - 50), cv::FONT_HERSHEY_DUPLEX, 1, color);
+            
+            
+            
+            cv::Point tracked;
+            tracked.x = center_tracked.x;
+            tracked.y = center_tracked.y;
+            cv::drawMarker(frame, tracked, color2, cv::MARKER_TRIANGLE_DOWN, 20, 5);
+            cv::putText(frame, "Tracked", cv::Point(tracked.x - 100, tracked.y - 100), cv::FONT_HERSHEY_DUPLEX, 1, color2);
+            if(tracked.x >= 345 && path == "../project_pm/src/pm_proj1/src/videoTennis.mp4"){
+                cv::putText(frame, "COLLISION", cv::Point(450, 30), cv::FONT_HERSHEY_DUPLEX, 1, color3);
+            }
+            if(tracked.x >= 170 && tracked.y >=553 && path == "../project_pm/src/pm_proj1/src/videoPlastic.mp4"){
+                cv::putText(frame, "COLLISION", cv::Point(900, 30), cv::FONT_HERSHEY_DUPLEX, 1, color3);
+            }
             center.x = com.x;
             center.y = com.y;
             center.z = 0;
-          
+
             center_ball_publisher.publish(center);
 
             Canny(frame_threshold, cont, thresh, thresh * 2);
-            vector<vector<Point>> contours;
-            vector<Vec4i> hierarchy;
-            findContours(cont, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
-            Mat drawing = Mat::zeros(cont.size(), CV_8UC3);
+            cv::cvtColor(cont,prin, cv::COLOR_GRAY2BGR);
+            std::vector<std::vector<cv::Point>> contours;
+            std::vector<cv::Vec4i> hierarchy;
+            findContours(cont, contours, hierarchy, cv::RETR_TREE, cv::CHAIN_APPROX_SIMPLE);
+            cv::Mat drawing = cv::Mat::zeros(cont.size(), CV_8UC3);
             for (size_t i = 0; i < contours.size(); i++)
             {
-                Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-                drawContours(frame, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
+                cv::Scalar color = cv::Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+                drawContours(frame_threshold, contours, (int)i, color, 2, cv::LINE_8, hierarchy, 0);
                 //////////////// Alinea E///////////////////
                 area = contourArea(contours[i]);
                 if (area > 150)
                 {
-                    std::cout << " Area: " << contourArea(contours[i]) << std::endl;
+                    vec.push_back(area);
                 }
             }
             
-
-            writer.write(frame);
-            writer.write(frame_threshold);
             // Show the frames
             imshow(window_capture_name, frame);
             imshow(window_detection_name, frame_threshold);
-            imshow(window_cont_name, cont);
+            imshow(window_cont_name, prin);
             moveWindow(window_capture_name, 800, 10);
             moveWindow(window_detection_name, 150, 250);
             moveWindow(window_cont_name, 800, 800);
             resizeWindow(window_capture_name, 950, 550);
             resizeWindow(window_detection_name, 600, 600);
             resizeWindow(window_cont_name, 950, 550);
- 
             
+
+            writer.write(prin);
+            writer2.write(frame);
             // Press Space Bar to continue, ESC to exit
             char c = (char)cv::waitKey(0);
             if (c == 27)
                 break;
-            
+
             ros::spinOnce();
             loop_rate.sleep();
-       }
-       char c = (char)cv::waitKey(0);
-            if (c == 27)
-                break;
-  }
-        // When everything is done, release the video capture and writer objects
-        cap.release();
-        writer.release();
+        }
+        char c = (char)cv::waitKey(0);
+        if (c == 27)
+            break;
+    }
+    double sum = 0;
+    int size = 1;
+    double avg_area;
+    for (int i = 0; i < vec.size(); i++)
+    {
+        sum += vec[i];
+        size++;
+    }
+    avg_area = sum / size;
+    std::cout << " Average Area: " << avg_area << std::endl;
+    // When everything is done, release the video capture and writer objects
+    cap.release();
+    writer.release();
+    writer2.release();
 
-        // Close all the frames
-        cv::destroyAllWindows();
+    // Close all the frames
+    cv::destroyAllWindows();
 
-  
-        return 0;
-    
+    return 0;
 }
--- src/pm_proj1/src/vtracker.cpp
+++ src/pm_proj1/src/vtracker.cpp
@@ -1,29 +1,38 @@
 #include "pm_proj1.h"
-
+namespace plt = matplotlibcpp;
 
 geometry_msgs::Vector3 center;
-   
+sensor_msgs::ImagePtr frame;
 
-void cbPose(const sensor_msgs::ImagePtr &msg_frame)
+void cbPose(const sensor_msgs::ImageConstPtr &msg_frame)
 {
-    if (!flag_first)
+    try
     {
         // Received center
-        // FAZER direiro o subscriber
-        ROS_WARN_STREAM("Inside callback!");
-        ROS_WARN_STREAM("center=(" << ball_center.x << "," << ball_center.y << ")");
-        flag_first = true;
+        cv::Mat img = cv_bridge::toCvShare(msg_frame, "bgr8")->image;
+
+        cv::imshow("Frame", cv_bridge::toCvShare(msg_frame, "bgr8")->image);
+
+        cv::waitKey(1);
+    }
+    catch (cv_bridge::Exception &e)
+    {
+        ROS_ERROR("ERRO");
     }
 }
 
 void cbPose2(const geometry_msgs::Vector3 &msg)
 {
-        // Received center
-        // FAZER direiro o subscriber
+    if(msg.x <0 || msg.y <0){
+        
+    }else{
         center.x = msg.x;
         center.y = msg.y;
-        ROS_WARN_STREAM("Inside callback!");
+        ROS_WARN_STREAM("Inside callback Obs!");
         ROS_WARN_STREAM("center=(" << msg.x << "," << msg.y << ")");
+        
+    }
+    
     
 }
 
@@ -31,92 +40,101 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "vtracker");
     ros::NodeHandle n_center;
+    ros::NodeHandle n_frame;
+    cv::namedWindow("Frame");
+    image_transport::ImageTransport it(n_frame);
     // Create the node handles to establish the program as a ROS node
-    ros::Subscriber sub = n_center.subscribe("vloarder/ball_center",1,cbPose2);
+    ros::Subscriber sub = n_center.subscribe("vloarder/ball_center", 1, cbPose2);
+    image_transport::Subscriber sub_frame = it.subscribe("vloarder/frame", 1, cbPose);
 
+    ros::Publisher center_ball_publisher_tracked;
+    center_ball_publisher_tracked = n_center.advertise<geometry_msgs::Vector3>("vtracker/ball_center_tracker", 1000);
 
-/////////////////////////EXERCISE 2////////////////////////////
+    /////////////////////////EXERCISE 2////////////////////////////
+
+    float fps = 30;
+
+    cv::KalmanFilter kalman_fil(4, 2);
+
+    float transitionMatrixValues[4][4] = {{1, 0, 1 / fps, 0},
+                                          {0, 1, 0, 1 / fps},
+                                          {0, 0, 1, 0},
+                                          {0, 0, 0, 1}};
+    kalman_fil.transitionMatrix = cv::Mat(4, 4, CV_32F, transitionMatrixValues);
+
+    float measurementMatrixValues[2][4] = {{1, 0, 0, 0},
+                                           {0, 1, 0, 0}};
+    kalman_fil.measurementMatrix = cv::Mat(2, 4, CV_32F, measurementMatrixValues);
+
+    float processNoiseMatrix[4][4] = {{1e-3, 0, 0, 0},
+                                      {0, 1e-3, 0, 0},
+                                      {0, 0, 1e-3, 0},
+                                      {0, 0, 0, 1e-3}};
+    kalman_fil.processNoiseCov = cv::Mat(4, 4, CV_32F, processNoiseMatrix);
+
+    float measurementNoiseMatrix[2][2] = {{1e-4, 0},
+                                          {0, 1e-4}};
+    kalman_fil.measurementNoiseCov = cv::Mat(2, 2, CV_32F, measurementNoiseMatrix);
+
+    float errorCovPostMatrix[4][4] = {{0.5, 0, 0, 0},
+                                      {0, 0.5, 0, 0},
+                                      {0, 0, 0.5, 0},
+                                      {0, 0, 0, 0.5}};
+    kalman_fil.errorCovPost = cv::Mat(4, 4, CV_32F, errorCovPostMatrix);
+
+    cv::Mat mp(2, 1, CV_32F, cv::Scalar::all(0));
+    std::vector<int> predicted_x;
+    std::vector<int> predicted_y;
+    std::vector<double> observation_x;
+    std::vector<double> observation_y;
+    cv::Mat tp;
+
+    while (ros::ok())
+    {
+        if (center.x > -1)
+        {
+            mp.at<float>(0, 0) = (float)center.x;
+            mp.at<float>(1, 0) = (float)center.y;
+            kalman_fil.correct(mp);
+        }
+        else
+        {
+            mp.at<float>(0, 0) = 0;
+            mp.at<float>(1, 0) = 0;
+        }
+
+        tp = kalman_fil.predict();
+
+        predicted_x.push_back((int)tp.at<float>(0));
+        predicted_y.push_back((int)tp.at<float>(1));
+
+        observation_x.push_back(center.x);
+        observation_y.push_back(center.y);
+
+        geometry_msgs::Vector3 center_tracked;
+        center_tracked.x = tp.at<float>(0);
+        center_tracked.y = tp.at<float>(1);
+        center_tracked.z = 0;
+
+        center_ball_publisher_tracked.publish(center_tracked);
+        
+        ros::spinOnce();
+    }
     
-    // std::vector<float> observation_x;
-    // std::vector<float> observation_y;
-    
-    // int fps = 30;
-    
-    // float xinit = observation_x[0];
-    // float yinit = observation_y[0];
 
-    // float vxinit = (observation_x[1] - observation_x[0]) * fps;
-    // float vyinit = (observation_y[1] - observation_y[0]) * fps;
-
-    // cv::KalmanFilter kalman_fil(4,2);
-
-    // kalman_fil.statePost.at<float>(0) = (float)xinit;
-    // kalman_fil.statePost.at<float>(1) = (float)yinit;
-    // kalman_fil.statePost.at<float>(2) = (float)vxinit;
-    // kalman_fil.statePost.at<float>(3) = (float)vyinit;
-
-    // kalman_fil.statePre.at<float>(0) = (float)xinit;
-    // kalman_fil.statePre.at<float>(1) = (float)yinit;
-    // kalman_fil.statePre.at<float>(2) = (float)vxinit;
-    // kalman_fil.statePre.at<float>(3) = (float)vyinit;
-
-    // float transitionMatrixValues[4][4] = { {1, 0, 1/fps, 0}, 
-    //                                        {0, 1, 0, 1/fps},
-    //                                        {0, 0, 1, 0},
-    //                                        {0, 0, 0, 1} };
-    // kalman_fil.transitionMatrix = cv::Mat(4, 4, CV_32F, transitionMatrixValues);
-
-    // float measurementMatrixValues[2][4] = { {1, 0, 0, 0},
-    //                                         {0, 1, 0, 0} };
-    // kalman_fil.measurementMatrix = cv::Mat(2, 4, CV_32F, measurementMatrixValues);
-
-    // float processNoiseMatrix[4][4] = { {1e-3, 0, 0, 0}, 
-    //                                    {0, 1e-3, 0, 0},
-    //                                    {0, 0, 1e-3, 0},
-    //                                    {0, 0, 0, 1e-3} };
-    // kalman_fil.processNoiseCov = cv::Mat(4, 4, CV_32F, processNoiseMatrix);
-
-    // float measurementNoiseMatrix[2][2] = { {1e-4, 0},
-    //                                        {0, 1e-4} };
-    // kalman_fil.measurementNoiseCov = cv::Mat(2, 2, CV_32F, measurementNoiseMatrix);
-
-    // float errorCovPostMatrix[4][4] = { {0.5, 0, 0, 0},
-    //                                    {0, 0.5, 0, 0},
-    //                                    {0, 0, 0.5, 0},
-    //                                    {0, 0, 0, 0.5} };
-    // kalman_fil.errorCovPost = cv::Mat(4, 4, CV_32F, errorCovPostMatrix);
-
-
-    // cv::Mat mp(2, 1, CV_32F, cv::Scalar::all(0));
-    // std::vector<int> predicted_x;
-    // std::vector<int> predicted_y;
-
-    // for (int i = 0; i < observation_x.size(); i++) {
-    //     if (observation_x[i] > -1) {
-
-    //         mp.at<float>(0, 0) = (float)observation_x[i];
-    //         mp.at<float>(1, 0) = (float)observation_y[i];
-    //         kalman_fil.correct(mp);
-    //     }
-    //     else {
-    //         mp.at<float>(0, 0) = 0;
-    //         mp.at<float>(1, 0) = 0;
-    //     }
-
-    //     cv::Mat tp = kalman_fil.predict();
-    //     predicted_x.push_back((int)tp.at<float>(0));
-    //     predicted_y.push_back((int)tp.at<float>(1));
-    // }
-
-
-    // plt.figure(figsize=(16, 6));
-    // plt.plot(observation_x, observation_y, linestyle='', marker='x', color='g', label='observation');
-    // plt.plot(predicted_x, predicted_y, linestyle='', color='b', label='tracked');
-    // plt.legend(loc='lower right');s
-    // plt.xlabel('x');
-    // plt.ylabel('y');
-    // plt.show();
+    plt::figure(1);
+    plt::subplot(2,2,1);
+    plt::plot(observation_x, observation_y, "g-", {{"label", "Observation"}});
+    plt::subplot(2,2,2);
+    plt::plot(predicted_x, predicted_y, "b*", {{"label", "Predicted"}});
+    plt::legend("lower right");
+    plt::xlabel("x");
+    plt::ylabel("y");
+    plt::show();
     // Spin - Infinite loop to ask ROS to read all pending callbacks
-    ros::spin();
+
+    cv::destroyWindow("Frame");
+    // Close the file
+
     return 0;
 }
