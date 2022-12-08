5892da1428aa588527648bcbb8dfd9a2d6fc8faf
Thaidy Deguchi <78708477+deguchi-04@users.noreply.github.com>
2022-12-08 14:05:53 +0000

    Add files via upload

--- src/pm_proj1/include/pm_proj1.h
+++ src/pm_proj1/include/pm_proj1.h
@@ -12,14 +12,14 @@
 #include <turtlesim/Pose.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-
+#include<matplotlibcpp.h>
 /// Global vars
 
 // name of the windows
 const cv::String window_capture_name = "Video Capture";
 const cv::String window_detection_name = "Object Detection";
 const cv::String window_cont_name = "Contours Detection";
-
+std::string name, name2;
 std::string path = "../project_pm/src/pm_proj1/src/videoPlastic.mp4";
 cv::RNG rng(12345);
 cv::Point ball_center;
@@ -28,16 +28,14 @@ bool flag_first;
 int thresh = 80;
 int meanColor = 0;
 int stdev = 0;
-double area = 0;
-
-const int max_value_H = 360 / 2;
-const int max_value = 255;
-
-int low_H = 0, low_S = 0, low_V = 0;
-int high_H = max_value_H, high_S = max_value, high_V = max_value;
-
-sensor_msgs::ImagePtr msg;
 
 
+int lowH = 0;
+int lowS = 0;
+int lowV = 0;
+int highH = 180;
+int highS = 255;
+int highV = 255;
 
+std::vector<double> vec;
 #endif
